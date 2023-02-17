c813cd0a2630b50e0831c62b35a5a637bb59c149
nyeliep <gielpalnyeliep@gmail.com>
2023-02-17 10:55:13 +0300

    commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,53 @@
+fun main(args: Array<String>){
+    val text = "akirachix"
+    println(text[0]+""+text[2]+""+text[3])
+
+
+    val name = "Tek "
+    val age = 21
+    val stnc = "Hi my name is "+name+" and I am "+age+" old."
+    println(stnc)
+
+    val wonder = "Northern lights"
+    wonder.length
+    println(wonder.length)
+
+
+    var me = "Nyeliep Giel"
+    if (me.equals("Nyeliep Giel"))
+        println("That's me")
+
+
+    else{
+        println("I don't know who that is")
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
